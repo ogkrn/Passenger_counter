7046fa5db9f00cb9434532751f63dc64ca45700a
Karan Joshi <160716100+ogkrn@users.noreply.github.com>
2025-08-02 15:01:29 +0000

    increment working on the web app

--- script.js
+++ script.js
@@ -2,9 +2,15 @@
 
 //initialize count from 0
 //listen for click on increment button
-//increase the count when button is clicked
-// change the count value in the html
+//increase the count when button is clicked (consol
+// e.log it)
+// change the count value in the html to reflect it
+let counted = 0
+let counting = document.getElementById("count")
 
 function increase(){
-console.log("Button clicked");
+    console.log("Button clicked");
+    counted = counted+1
+    counting.innerText=counted
+    console.log(counted)
 }
