817ab4ebded1f27d90526586cbba2a0190c93f23
Karan Joshi <160716100+ogkrn@users.noreply.github.com>
2025-08-02 14:57:22 +0000

    html,css,js and images added

---
+++ js.js
@@ -0,0 +1,7 @@
+
+function countdown(){
+    let one = 1;
+    let two = 2;
+    console.log(one+two);
+}
+countdown();
---
+++ script.js
@@ -0,0 +1,10 @@
+// document.getElementById("count").innerText=5
+
+//initialize count from 0
+//listen for click on increment button
+//increase the count when button is clicked
+// change the count value in the html
+
+function increase(){
+console.log("Button clicked");
+}
